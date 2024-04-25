9ebff771431f7eb756952cfa00f0e33a9748e9d9
Kaywuyep <wuyepkate@gmail.com>
2024-04-25 09:52:21 +0000

    task

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+export default class ClassRoom {
+  // Constructor method using maxStudentsSize as an argument
+  constructor(maxStudentsSize) {
+    // Assign the value of maxStudentsSize to the private property _maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
