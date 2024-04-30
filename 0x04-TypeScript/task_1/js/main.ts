ee5ef95e609ee25d4c52fcf052f178979cd354d1
Kaywuyep <wuyepkate@gmail.com>
2024-04-30 11:19:14 +0100

    task

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,16 @@
-cd 
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    readonly fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+console.log(teacher3)
