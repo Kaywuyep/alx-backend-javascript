123f9d1fdf84b5e8f2e5c8d50d4668ca8040d14c
Kaywuyep <wuyepkate@gmail.com>
2024-04-30 09:07:35 +0000

    task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 // It accepts two arguments: a set (Set) and a startString (String)
 export default function cleanSet(set, startString) {
-   if (startString === undefined || startString.length === 0) {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
   // Use filter and map to process matching set values
   return [...set]
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length)) // Extract remaining part
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length)) // Extract remaining part
     .join('-');
 }
