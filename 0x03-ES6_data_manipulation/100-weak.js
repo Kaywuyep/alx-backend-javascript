fe722b7b9f4924665b6ad1e2e262568d7a15e330
Kaywuyep <wuyepkate@gmail.com>
2024-04-30 09:19:26 +0000

    task

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,8 +2,12 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let called = 0;
-  if (weakMap.get(endpoint)) endpointCall = weakMap.get(endpoint);
-  weakMap.set(endpoint, endpointCall + 1);
-  if (endpointCall + 1 >= 5) throw new Error('Endpoint load is high');
+  // Initialize endpointCall to 0 if not found in the WeakMap
+  let endpointCall = weakMap.get(endpoint) || 0;
+  endpointCall += 1; // Increment the number of calls
+  weakMap.set(endpoint, endpointCall); // Update the number of calls in the WeakMap
+
+  if (endpointCall >= 5) { // Check if the number of calls is greater than or equal to 5
+    throw new Error('Endpoint load is high');
+  }
 }
